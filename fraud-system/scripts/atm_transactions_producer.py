ff8504056da12ab6ad2ef4398d30f9a469ed4b1d
FawziLinggo <fawzilinggo@gmail.com>
2023-11-19 19:04:36 +0700

    main

--- fraud-system/scripts/atm_transactions_producer.py
+++ fraud-system/scripts/atm_transactions_producer.py
@@ -1,6 +1,5 @@
 import json
 import socket
-import subprocess
 from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
 import time
@@ -8,15 +7,13 @@ import os
 
 # GET ENVIRONMENT VARIABLES
 try:
-  KAFKA_BROKERS = os.environ['KAFKA_BROKERS']
-  SCHEMA_REGISTRY = os.environ['SCHEMA_REGISTRY']
-  CLIENT_ID = os.environ['CLIENT_ID']
+    KAFKA_BROKERS = os.environ['KAFKA_BROKERS']
+    SCHEMA_REGISTRY = os.environ['SCHEMA_REGISTRY']
+    CLIENT_ID = os.environ['CLIENT_ID']
 except KeyError as e:
-  KAFKA_BROKERS = "localhost:9092"
-
-  SCHEMA_REGISTRY = "http://localhost:8081"
-
-  CLIENT_ID = "atm-transactions-producer"
+    KAFKA_BROKERS = "ol8-21.localdomain:9092"
+    SCHEMA_REGISTRY = "http://ol8-21.localdomain:8081"
+    CLIENT_ID = "atm-transactions-producer"
 
 conf = {
     'bootstrap.servers': KAFKA_BROKERS,
@@ -58,27 +55,27 @@ key_schema_str = """
 value_schema = avro.loads(value_schema_str)
 key_schema = avro.loads(key_schema_str)
 
-avroProducer  = AvroProducer(conf, default_key_schema=key_schema, default_value_schema=value_schema)
+avroProducer = AvroProducer(conf, default_key_schema=key_schema, default_value_schema=value_schema)
 
-proc = subprocess.Popen(["nc -luv 6900"], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# Create a UDP socket
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket.bind(('0.0.0.0', 6900))
 
 while True:
-  response = proc.stdout.readline()
-  jsonString = str(response, 'utf-8')
-  print(jsonString)
-  if jsonString != '':
-        
-    value = json.loads(jsonString)
-    key = {"transaction_id": value['transaction_id']}
-    try:
-        avroProducer.produce(topic='ATM_TRANSACTION', value=value, key=key)
-        print(f"Produced record to topic {value}")
-    except Exception as e:
-        print(f"Exception while producing record value - {value} to topic - 'ATM_TRANSACTION': {e}")
-    else:
-        print(f"key - {key}")
+    data, addr = udp_socket.recvfrom(1024)
+    jsonString = data.decode('utf-8')
+    print(jsonString)
 
-    avroProducer.flush()
+    if jsonString != '':
+        value = json.loads(jsonString)
+        key = {"transaction_id": value['transaction_id']}
 
-# Terminate process
-proc.terminate()
+        try:
+            avroProducer.produce(topic='ATM_TRANSACTION', value=value, key=key)
+            print(f"Produced record to topic {value}")
+        except Exception as e:
+            print(f"Exception while producing record value - {value} to topic - 'ATM_TRANSACTION': {e}")
+        else:
+            print(f"key - {key}")
+
+        avroProducer.flush()
